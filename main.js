7de4d068c89fa3cef33c3292a910669ef8266af0
Vihaanisthebest <129866559+Vihaanisthebest@users.noreply.github.com>
2024-02-09 18:36:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+rightWristX = 0;
+rightWristY = 0;
+
+scoreRightWrist = 0;
+
 function setup()
 {
     canvas = createCanvas(450, 400);
@@ -8,14 +13,33 @@ function setup()
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
     image(video, 0, 0, 450, 400);
+
+    if (scoreRightWrist > 0.2)
+    {
+        fill("#3467eb");
+        stroke("#fa3123");
+        circle(rightWristX, rightWristY, 25);
+    }
 }
 
 function modelLoaded()
 {
     console.log("Model Is Loaded!")
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        
+        scoreRightWrist =  results[0].pose.keypoints[10].score;
+    }
 }
